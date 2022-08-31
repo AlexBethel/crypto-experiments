3d0916c37dc3e19b86568a010355eb83f407cc3c
Alex Bethel <abethel@cs.nmt.edu>
2022-08-30 21:16:37 -0600

    Feistel network OwO

--- src/feistel.rs
+++ src/feistel.rs
@@ -7,6 +7,18 @@ pub fn feistel(
     rounds: usize,
     f: impl Fn(usize, &[bool]) -> Vec<bool>,
     input: &[bool],
+    encrypt: bool,
 ) -> Vec<bool> {
-    todo!()
+    let mut state = input.to_owned();
+    for i in (0..rounds).map(|i| if encrypt { i } else { rounds - i - 1 }) {
+        let (left, right) = state.split_at(state.len() / 2);
+        let (new_left, new_right) = (
+            left.iter().zip(f(i, right).iter()).map(|(&l, &r)| l ^ r),
+            left.iter().cloned(),
+        );
+
+        state = new_left.chain(new_right).collect();
+    }
+
+    state
 }
