c62ee4c8cbef542d6e006786fec40a4203e0296d
shastro <guitarshughes@hotmail.com>
2022-08-30 20:46:34 -0600

    Starting permuations

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+// Initial permutation
+
+fn ip(block: Vec<bool>) -> Vec<bool> {
+
 }
